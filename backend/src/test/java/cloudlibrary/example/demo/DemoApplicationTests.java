5b098f183a545f114ea8570935be88616ecbae4c
danielb113d1a <danielb11023d1a@gmail.com>
2025-10-27 01:26:48 +0100

    d
    d

--- backend/src/test/java/cloudlibrary/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/cloudlibrary/example/demo/DemoApplicationTests.java
@@ -1,21 +1,13 @@
 package cloudlibrary.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Añadimos 'exclude' para evitar que intente configurar la BBDD
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }

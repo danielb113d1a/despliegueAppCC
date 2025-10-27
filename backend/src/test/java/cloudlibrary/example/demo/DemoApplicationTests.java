89626507ebc401283f10fc46cff4662a9f5202cc
danielb113d1a <danielb11023d1a@gmail.com>
2025-10-27 02:03:16 +0100

    mucho
    mucho

--- backend/src/test/java/cloudlibrary/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/cloudlibrary/example/demo/DemoApplicationTests.java
@@ -1,23 +1,13 @@
 package cloudlibrary.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Asegúrate de importar estas
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration; // Asegúrate de importar estas
 import org.springframework.boot.test.context.SpringBootTest;
 
-// ESTA PARTE ES LA IMPORTANTE
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
 class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // Este test ahora solo comprueba que el contexto básico carga,
-        // sin intentar conectar a ninguna base de datos.
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
